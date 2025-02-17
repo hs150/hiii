3c2e746fa4e9f4b83e8d7276b778cb333fb77594
harsh <98870227+hs150@users.noreply.github.com>
2025-02-17 19:04:39 +0530

    Updated on Main.java
    After the login credentials update

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-// This is the Main Class for Airline System As of 16 feb 2025
 
 import java.util.Random;
 import java.util.Scanner;
@@ -8,6 +7,41 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         AirlineSystem system = new AirlineSystem();
 
+       // This process is for registration
+        System.out.println("Enter username:");
+        String username = scanner.next();
+
+        System.out.println("Enter password:");
+        String password = scanner.next();
+
+        // these are some line filling code
+
+        System.out.println("Enter name:");
+        String name = scanner.next();
+
+        System.out.println("Enter email:");
+        String email = scanner.next();
+
+        System.out.println("Enter phone number:");
+        String phoneNumber;
+        // A loop for checking phone number is valid or not
+        while (true) {
+            phoneNumber = scanner.next();
+            // Regular Expression is used
+            if (phoneNumber.matches("\\d{10}")) {
+                break;
+            } else {
+                System.out.println("Invalid phone number. Please enter a 10-digit phone number.");
+            }
+        }
+
+        System.out.println("You have been registered successfully. ");
+        System.out.println("Please login to BOOK tickets through our Airline. ");
+        // Running the login portal
+        AirlineSystem.Customer customer = system.new Customer(username, password, name, email, phoneNumber, "", "",0 );
+        customer.loginDetails();
+
+        // Displaying origin
         AirlineSystem.Origin origin = system.new Origin();
         origin.displayOrigins();
         int Choice;
@@ -26,12 +60,11 @@ public class Main {
             }
 
         }
-
-
+        // Displaying destination
         AirlineSystem.Destination destination = system.new Destination();
         destination.displayDestinations();
 
-
+        // Choosing destination and origin for our fare
         int choice;
         while (true) {
             System.out.println("Enter Your Destination By Choosing choice (1-6):");
@@ -48,39 +81,17 @@ public class Main {
             }
 
         }
+        // the loop which we earlier created work now
 
         String chosenDestination = destination.destinations[choice - 1];
         String chosenOrigin = origin.origins[Choice - 1];
 
-        System.out.println("Enter username:");
-        String username = scanner.next();
-
-        System.out.println("Enter password:");
-        String password = scanner.next();
-
-        System.out.println("Enter name:");
-        String name = scanner.next();
-
-        System.out.println("Enter email:");
-        String email = scanner.next();
-
-        System.out.println("Enter phone number:");
-        String phoneNumber;
-        while (true) {
-            phoneNumber = scanner.next();
-            if (phoneNumber.matches("\\d{10}")) {
-                break;
-            } else {
-                System.out.println("Invalid phone number. Please enter a 10-digit phone number.");
-            }
-        }
-
-
+        // Running a loop for fetching price
         int[] Price = { 5000 , 7000 , 8000 ,9000 ,2000};
         Random rand  = new Random();
         int selectedPrice= Price[rand.nextInt(Price.length)];
 
-        AirlineSystem.Customer customer = system.new Customer(username, password, name, email, phoneNumber, chosenDestination, chosenOrigin , selectedPrice);
+        customer = system.new Customer(username, password, name, email, phoneNumber, chosenDestination, chosenOrigin , selectedPrice);
         customer.displayDetails();
     }
 }
